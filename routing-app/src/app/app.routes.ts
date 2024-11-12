c2b4cb939c1f8eafd4910e38398778962cfa713e
Naveen Manjappa <NManjappa@manh.com>
2024-11-12 17:59:52 +0000

    Defining routes

--- routing-app/src/app/app.routes.ts
+++ routing-app/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
   { path:'first-component', component:FirstComponent },
-  { path:'second-component', component:SecondComponent }
+  { path:'second-component', component:SecondComponent },
+  { path: '',redirectTo:'/first-component',pathMatch:'full'},
+  { path: '**', component:PagenotfoundComponent}
 ];
