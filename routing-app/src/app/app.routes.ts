ef6e5da29b28b72f4b86ca6c6915eb29cbed0407
Naveen Manjappa <NManjappa@manh.com>
2024-11-13 07:14:56 +0000

    Defining child routes

--- routing-app/src/app/app.routes.ts
+++ routing-app/src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ChildAComponent } from './first/child-a/child-a.component';
+import { ChildBComponent } from './first/child-b/child-b.component';
 
 export const routes: Routes = [
-  { path:'first-component', component:FirstComponent },
-  { path:'second-component', component:SecondComponent },
+  { path:'first-component', title:'First Component', component:FirstComponent,
+    children: [
+      { path:'child-a', title:'Child A', component: ChildAComponent },
+      { path: 'child-b', title:'Child B', component: ChildBComponent }
+    ]
+   },
+  { path:'second-component',title:'Second Component', component:SecondComponent },
   { path: '',redirectTo:'/first-component',pathMatch:'full'},
-  { path: '**', component:PagenotfoundComponent}
+  { path: '**', component:PagenotfoundComponent }
 ];
