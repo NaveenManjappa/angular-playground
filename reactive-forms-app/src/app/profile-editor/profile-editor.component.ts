d6a7537505f6ad7f839011f512b490a1526e26c4
Naveen Manjappa <NManjappa@manh.com>
2024-11-28 12:42:05 +0000

    reactive-forms-app Using Nested forms

--- reactive-forms-app/src/app/profile-editor/profile-editor.component.ts
+++ reactive-forms-app/src/app/profile-editor/profile-editor.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './profile-editor.component.html',
   styleUrl: './profile-editor.component.css'
 })
@@ -12,7 +13,13 @@ export class ProfileEditorComponent {
 
   profileForm:FormGroup = new FormGroup({
     firstName: new FormControl(),
-    lastName: new FormControl()
+    lastName: new FormControl(),
+    address: new FormGroup({
+      street: new FormControl(),
+      city: new FormControl(),
+      state: new FormControl(),
+      zip: new FormControl()
+    })
   });
 
   saveUser(){
