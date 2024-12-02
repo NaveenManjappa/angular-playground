e12db505d16541dbeaa5be07d56f52eebc33ae00
Naveen Manjappa <NManjappa@manh.com>
2024-12-02 16:44:06 +0000

    template-forms-app Basic template driven form

---
+++ template-forms-app/src/app/actor-form/actor-form.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Actor } from '../actor';
+import { Form, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-actor-form',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './actor-form.component.html',
+  styleUrl: './actor-form.component.css'
+})
+export class ActorFormComponent {
+
+  skills = ['Method Acting','Singing','Dancing','Sword Fighting'];
+
+  model = new Actor(18,'Tom Cruise',this.skills[3],'CW Productions');
+
+  submitted = false;
+
+  onSubmit(actorForm:any) {
+    console.log(actorForm.form.controls);
+    this.submitted = true;
+  }
+
+  newActor() {
+    this.model = new Actor(42,'','')
+  }
+
+  showFormControls(form:any) {
+    return form && form.controls.name && form.controls.name.value;
+  }
+}
--- template-forms-app/src/app/app.component.ts
+++ template-forms-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ActorFormComponent } from './actor-form/actor-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ActorFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
