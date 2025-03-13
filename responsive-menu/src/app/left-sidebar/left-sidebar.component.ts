70e12dcc3c00e98ff356a533da5bc2946623db96
Naveen Manjappa <NManjappa@manh.com>
2025-03-13 07:42:15 +0000

    responsive-menu Implement the navigation bar with styling

--- responsive-menu/src/app/left-sidebar/left-sidebar.component.ts
+++ responsive-menu/src/app/left-sidebar/left-sidebar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.css'
 })
 export class LeftSidebarComponent {
-
+items = [
+  { routeLink:'dashboard',label:'Dashboard',icon:'fa-solid fa-house'},
+  { routeLink:'pages',label:'Pages',icon:'fa-solid fa-file'},
+  { routeLink:'products',label:'Products',icon:'fa-solid fa-box-open'},
+  { routeLink:'settings',label:'Settings',icon:'fa-solid fa-gear'}
+];
 }
