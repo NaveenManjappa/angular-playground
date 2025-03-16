40f21df6f694e66d4c711b72eec3a04d0927a718
Naveen Manjappa <NManjappa@manh.com>
2025-03-16 18:46:42 +0000

    ng-searchbar Starter project

---
+++ ng-searchbar/src/app/article.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ArticleInterface } from './article.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+  http = inject(HttpClient);
+  getArticles(searchValue:string):Observable<ArticleInterface[]>{
+    return this.http.get<ArticleInterface[]>(`http://localhost:3000/articles?title_like=${searchValue}`);
+  }
+}
