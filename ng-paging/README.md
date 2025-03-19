c4d041069948f263e02fe0dc41beb52737a4bb3f
Naveen Manjappa <NManjappa@manh.com>
2025-03-19 07:20:18 +0000

    ng-paging Implement paging component with styles

--- ng-paging/README.md
+++ ng-paging/README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+
+## Reference
+This is the project created from Youtube video: https://youtu.be/S_wTb4pP_6Q?si=o59H9yZaCBUsI43w&t=9931
+https://github.com/monsterlessonsacademy/monsterlessonsacademy/tree/265-angular-pagination
--- ng-paging/src/app/pagination/pagination.component.ts
+++ ng-paging/src/app/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'pagination',
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.css'
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnInit{
+ 
+  @Input() currentPage:number = 1;
+  @Input() total:number = 0;
+  @Input() limit:number = 20;
+  @Output() changePage = new EventEmitter<number>();
+
+  pages:number[] = [];
+
+  ngOnInit(): void {
+    const pagesCount = Math.ceil(this.total / this.limit);    
+    this.pages = this.range(1,pagesCount);
+    console.log(this.pages);
+  }
+
+  range(start:number,end:number):number[] {
+    return [...Array(end).keys()].map(el => el+start);
+  }
 
 }
