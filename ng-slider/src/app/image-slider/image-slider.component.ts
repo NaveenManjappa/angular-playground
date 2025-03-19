eda6933f1c910cbc0a3d6b8e8ab7ec67fd9ae65f
Naveen Manjappa <NManjappa@manh.com>
2025-03-19 06:36:48 +0000

    ng-slider Implement move between slides

--- ng-slider/src/app/image-slider/image-slider.component.ts
+++ ng-slider/src/app/image-slider/image-slider.component.ts
@@ -14,4 +14,24 @@ currentIndex:number = 0;
 getCurrentSlideUrl():string {
   return `url('${this.slides[this.currentIndex].url}')`;
 }
+
+goToNext():void {    
+  this.currentIndex = this.isLastSlide() ? 0 : this.currentIndex + 1;
+}
+
+goToPrevious():void {
+  this.currentIndex = this.isFirstSlide() ? this.slides.length - 1 : this.currentIndex - 1;
+}
+
+isLastSlide():boolean {
+  return this.currentIndex === this.slides.length - 1;
+}
+
+isFirstSlide():boolean {
+  return this.currentIndex === 0;
+}
+
+goToSlide(slideIndex:number):void {
+  this.currentIndex = slideIndex;
+}
 }
