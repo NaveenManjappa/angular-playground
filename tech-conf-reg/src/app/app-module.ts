be1632e5a7a9460981774781994b99ff055e9e00
Naveen Manjappa <NManjappa@manh.com>
2025-10-01 16:26:35 +0100

    tech-conf-reg Initial project setup

---
+++ tech-conf-reg/src/app/app-module.ts
@@ -0,0 +1,20 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { App } from './app';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
